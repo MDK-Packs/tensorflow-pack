b954a98f76f6d7da5e41e9b7805f38ce0d2d1ed5
Matthias <matthias.hertel@arm.com>
2021-01-16 13:40:30 +0100

    Adding tensorflow make related jobs

--- tensorflow-build/prep.sh
+++ tensorflow-build/prep.sh
@@ -1,12 +1,10 @@
-rm *.lst
-cd ../tensorflow
-make -f tensorflow/lite/micro/tools/make/Makefile -f ../tensorflow-build/make/print.mak TAGS= TARGET=cortex_m_generic_makefile printvar-tagged-MICROLITE_CC_SRCS > ../tensorflow-build/srcs.lst
-make -f tensorflow/lite/micro/tools/make/Makefile -f ../tensorflow-build/make/print.mak TAGS= TARGET=cortex_m_generic_makefile printvar-tagged-MICROLITE_CC_HDRS > ../tensorflow-build/hdrs.lst
-make -f tensorflow/lite/micro/tools/make/Makefile -f ../tensorflow-build/make/print.mak TAGS=cmsis-nn TARGET=cortex_m_generic_makefile printvar-tagged-MICROLITE_CC_SRCS > ../tensorflow-build/srcs.cmsis-nn.lst
-make -f tensorflow/lite/micro/tools/make/Makefile -f ../tensorflow-build/make/print.mak TAGS=cmsis-nn TARGET=cortex_m_generic_makefile printvar-tagged-MICROLITE_CC_HDRS > ../tensorflow-build/hdrs.cmsis-nn.lst
-make -f tensorflow/lite/micro/tools/make/Makefile -f ../tensorflow-build/make/print.mak TAGS=ethos-u TARGET=cortex_m_generic_makefile printvar-tagged-MICROLITE_CC_SRCS > ../tensorflow-build/srcs.ethos.lst
-make -f tensorflow/lite/micro/tools/make/Makefile -f ../tensorflow-build/make/print.mak TAGS=ethos-u TARGET=cortex_m_generic_makefile printvar-tagged-MICROLITE_CC_HDRS > ../tensorflow-build/hdrs.ethos.lst
-make -f tensorflow/lite/micro/tools/make/Makefile -f ../tensorflow-build/make/print.mak TAGS= TARGET=cortex_m_generic_makefile printvar-tagged-MICROLITE_TEST_SRCS > ../tensorflow-build/srcs.test.lst
-make -f tensorflow/lite/micro/tools/make/Makefile -f ../tensorflow-build/make/print.mak TAGS= TARGET=cortex_m_generic_makefile printvar-tagged-MICROLITE_TEST_HDRS > ../tensorflow-build/hdrs.test.lst
+make -f /workspace/host/tensorflow/lite/micro/tools/make/Makefile -f /workspace/host/tensorflow-pack/tensorflow-build/make/print.mak TAGS= TARGET=cortex_m_generic_makefile printvar-tagged-MICROLITE_CC_SRCS > /workspace/host/tensorflow-pack/tensorflow-build/srcs.lst
+make -f /workspace/host/tensorflow/lite/micro/tools/make/Makefile -f /workspace/host/tensorflow-pack/tensorflow-build/make/print.mak TAGS= TARGET=cortex_m_generic_makefile printvar-tagged-MICROLITE_CC_HDRS > /workspace/host/tensorflow-pack/tensorflow-build/hdrs.lst
+make -f /workspace/host/tensorflow/lite/micro/tools/make/Makefile -f /workspace/host/tensorflow-pack/tensorflow-build/make/print.mak TAGS=cmsis-nn TARGET=cortex_m_generic_makefile printvar-tagged-MICROLITE_CC_SRCS > /workspace/host/tensorflow-pack/tensorflow-build/srcs.cmsis-nn.lst
+make -f /workspace/host/tensorflow/lite/micro/tools/make/Makefile -f /workspace/host/tensorflow-pack/tensorflow-build/make/print.mak TAGS=cmsis-nn TARGET=cortex_m_generic_makefile printvar-tagged-MICROLITE_CC_HDRS > /workspace/host/tensorflow-pack/tensorflow-build/hdrs.cmsis-nn.lst
+make -f /workspace/host/tensorflow/lite/micro/tools/make/Makefile -f /workspace/host/tensorflow-pack/tensorflow-build/make/print.mak TAGS=ethos-u TARGET=cortex_m_generic_makefile printvar-tagged-MICROLITE_CC_SRCS > /workspace/host/tensorflow-pack/tensorflow-build/srcs.ethos.lst
+make -f /workspace/host/tensorflow/lite/micro/tools/make/Makefile -f /workspace/host/tensorflow-pack/tensorflow-build/make/print.mak TAGS=ethos-u TARGET=cortex_m_generic_makefile printvar-tagged-MICROLITE_CC_HDRS > /workspace/host/tensorflow-pack/tensorflow-build/hdrs.ethos.lst
+make -f /workspace/host/tensorflow/lite/micro/tools/make/Makefile -f /workspace/host/tensorflow-pack/tensorflow-build/make/print.mak TAGS= TARGET=cortex_m_generic_makefile printvar-tagged-MICROLITE_TEST_SRCS > /workspace/host/tensorflow-pack/tensorflow-build/srcs.test.lst
+make -f /workspace/host/tensorflow/lite/micro/tools/make/Makefile -f /workspace/host/tensorflow-pack/tensorflow-build/make/print.mak TAGS= TARGET=cortex_m_generic_makefile printvar-tagged-MICROLITE_TEST_HDRS > /workspace/host/tensorflow-pack/tensorflow-build/hdrs.test.lst
 
 
