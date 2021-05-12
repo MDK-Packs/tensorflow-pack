38bb6b832ca19b218c506f8949934abea5403192
Matthias Hertel <matthias.hertel@arm.com>
2021-05-12 17:23:39 +0200

    Updating examples for smoke tests

---
+++ examples/hello_world/Platform_FVP/RTE/_hello_world.gcc/RTE_Components.h
@@ -0,0 +1,33 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'hello_world.gcc' 
+ * Target:  'hello_world.gcc' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "SSE300MPS3.h"
+
+/* tensorflow::Data Exchange:Serialization:flatbuffers:tensorflow:1.12.0 */
+#define RTE_DataExchange_Serialization_flatbuffers     /* flatbuffers */
+/* tensorflow::Data Processing:Math:gemmlowp fixed-point:tensorflow:1.0.0 */
+#define RTE_DataExchange_Math_gemmlowp     /* gemmlowp */
+/* tensorflow::Data Processing:Math:kissfft:tensorflow:1.4.5 */
+#define RTE_DataExchange_Math_kissfft     /* kissfft */
+/* tensorflow::Data Processing:Math:ruy:tensorflow:1.12.0 */
+#define RTE_DataProcessing_Math_ruy     /* ruy */
+/* tensorflow::Machine Learning:TensorFlow:Kernel:CMSIS-NN:0.1.20210416 */
+#define RTE_ML_TF_LITE     /* TF */
+/* tensorflow::Machine Learning:TensorFlow:Testing:0.1.20210416 */
+#define RTE_ML_TF_LITE     /* TF */
+
+
+#endif /* RTE_COMPONENTS_H */
