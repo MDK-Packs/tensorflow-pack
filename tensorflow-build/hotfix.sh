8e424195bbf0f2781ca11855aa54511673020137
Matthias Hertel <matthias.hertel@arm.com>
2021-06-10 15:14:00 +0200

    Update hotfix.sh

--- tensorflow-build/hotfix.sh
+++ tensorflow-build/hotfix.sh
@@ -7,9 +7,13 @@ sed -i 's/<LIST>/<LIST>tensorflow\/lite\/micro\/cortex_m_generic\/debug_log_call
 
 # Remove Generic Debug_log.cc from Kernel Utils
 sed -i 's/tensorflow\/lite\/micro\/debug_log.cc/ /' util_srcs.lst
-sed -i 's/tensorflow\/lite\/micro\/system_setup.cc/ /' hdrs.cmsis-nn.lst
 
 # Remove conv_test_common.cc from all lists
 sed -i 's/tensorflow\/lite\/micro\/kernels\/conv_test_common.cc / /' /workspace/host/tensorflow-pack/tensorflow-build/hdrs.cmsis-nn.lst
 sed -i 's/tensorflow\/lite\/micro\/kernels\/conv_test_common.cc / /' /workspace/host/tensorflow-pack/tensorflow-build/srcs.cmsis-nn.lst
 sed -i 's/tensorflow\/lite\/micro\/kernels\/conv_test_common.cc / /' /workspace/host/tensorflow-pack/tensorflow-build/srcs.lst
+
+# Remove system_setup.cc from all lists
+sed -i 's/tensorflow\/lite\/micro\/kernels\/system_setup.cc / /' /workspace/host/tensorflow-pack/tensorflow-build/hdrs.cmsis-nn.lst
+sed -i 's/tensorflow\/lite\/micro\/kernels\/system_setup.cc / /' /workspace/host/tensorflow-pack/tensorflow-build/srcs.cmsis-nn.lst
+sed -i 's/tensorflow\/lite\/micro\/kernels\/system_setup.cc / /' /workspace/host/tensorflow-pack/tensorflow-build/srcs.lst
