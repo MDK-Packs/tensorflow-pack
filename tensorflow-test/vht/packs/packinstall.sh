879c42e6d821aeef0664a4b29fb37a72e1e50569
Matthias Hertel <matthias.hertel@arm.com>
2022-02-28 16:50:24 +0100

    Update packinstall.sh

--- tensorflow-test/vht/packs/packinstall.sh
+++ tensorflow-test/vht/packs/packinstall.sh
@@ -5,3 +5,6 @@ cat packlist
 #sed -i 's/^/file:\/\//' packlist
 #cat packlist
 cpackget pack add -f packlist -a
+cpackget pack add https://www.keil.com/pack/ARM.CMSIS.5.8.0.pack -a
+cpackget pack add https://keilpack.azureedge.net/pack/ARM.V2M_MPS3_SSE_300_BSP.1.2.0.pack -a
+
