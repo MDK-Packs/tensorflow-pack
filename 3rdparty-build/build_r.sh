3a7c52851f045118f99215df3e0f7bed5ef6f9bf
Matthias Hertel <matthias.hertel@arm.com>
2022-05-31 19:47:28 +0200

    3rd Party rc support

--- 3rdparty-build/build_r.sh
+++ 3rdparty-build/build_r.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "Starting 3rd-Party builds for revision :" $1
+echo "Starting 3rd-Party builds for revision :" $1 $2
 
 cd ./tensorflow-pack/3rdparty-build
 
@@ -12,19 +12,19 @@ if [ ! -d "./src" ]; then
 fi
 
 # Build Ethos-U driver pack
-python3 gen_pack.py --path ethos-u-driver --version 1.$1
+python3 gen_pack.py --path ethos-u-driver --version 1.$1  --candidate_rev=$2 
 cp ./ethos-u-driver/build/*.pack ./
 # Build flatbuffers pack
-python3 gen_pack.py --path flatbuffers --version 1.$1
+python3 gen_pack.py --path flatbuffers --version 1.$1 --candidate_rev=$2 
 cp ./flatbuffers/build/*.pack ./
 # Build gemmlowp pack
-python3 gen_pack.py --path gemmlowp --version 1.$1
+python3 gen_pack.py --path gemmlowp --version 1.$1 --candidate_rev=$2 
 cp ./gemmlowp/build/*.pack ./
 # Build ruy pack
-python3 gen_pack.py --path ruy --version 1.$1
+python3 gen_pack.py --path ruy --version 1.$1 --candidate_rev=$2 
 cp ./ruy/build/*.pack ./
 # Build kissfft pack
-python3 gen_pack.py --path kissfft --version 1.$1
+python3 gen_pack.py --path kissfft --version 1.$1 --candidate_rev=$2 
 cp ./kissfft/build/*.pack ./
 
 
