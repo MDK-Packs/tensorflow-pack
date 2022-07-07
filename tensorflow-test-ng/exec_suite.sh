2b722cdd6a95124c11431369f8e2b9121f4abacc
Matthias Hertel <matthias.hertel@arm.com>
2022-07-07 11:01:01 +0200

    Update exec_suite.sh

--- tensorflow-test-ng/exec_suite.sh
+++ tensorflow-test-ng/exec_suite.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 
+counter = 0
 # Iterate through all folders in ./gen 
 # and generate test projects
 for folder in ./gen/*; do
     # increment counter
-    ((counter++))
-    echo Test $counter - $folder
+    echo Package $counter $folder
+    ((counter=counter+1))
     # Generate test project
     csolution convert -s $folder/Validation.csolution.yml -o $folder/
 done
 
+counter = 0
 for folder in ./gen/*; do
+    echo Package $counter $folder
+    ((counter=counter+1))
     for target in $folder/*; do
         #find .cprj file in target folder
-        cprj_file=$(find $target -name "*.cprj")
+        cprj_file=$(find $target -name "*.cprj")  
         echo "Building: " $cprj_file
         cbuild $cprj_file > $target/build.log
         python3 avh_exec_test.py --project=$cprj_file > $target/test_result.log
