302a2ee40f2bf9c69dd9c08bb3ed790eee59cf54
Matthias Hertel <matthias.hertel@arm.com>
2024-10-01 07:57:38 +0000

    Removed github API key requirement from get_releases.py

--- tensorflow-build/get_releases.py
+++ tensorflow-build/get_releases.py
@@ -1,22 +1,34 @@
-from github import Github
-import datetime
-import sys
 
-#Get first parameter from command line
-git_token = sys.argv[1]
+import requests
+from datetime import datetime
 
-
-G = Github(git_token)  # Put your GitHub token here
-repo = G.get_repo('MDK-Packs/tensorflow-pack')
-releases = repo.get_releases()
-
-releases = releases    
-
-for release in releases:
-    if 'nightly' in release.title:
-        continue
-    print("<release version=\""+release.title+"\" url=\"https://github.com/MDK-Packs/tensorflow-pack/releases/download/"+release.tag_name+"/tensorflow.tensorflow-lite-micro."+release.tag_name+".pack\" date=\""+release.created_at.strftime('%Y-%m-%d')+"\">" + release.body + "</release>")
-
-
+def get_release_history():
+    url = f"https://api.github.com/repos/MDK-PACKS/tensorflow-pack/releases"
+    releases = []
+    page = 1
     
+    while True:
+        response = requests.get(f"{url}?page={page}&per_page=100")
+        if response.status_code == 200:
+            page_releases = response.json()
+            if not page_releases:
+                break
+            releases.extend(page_releases)
+            page += 1
+        else:
+            print(f"Error fetching releases: {response.status_code}")
+            break
+    
+    return releases
 
+release_history = get_release_history()
+
+for release in release_history:
+    if 'nightly' in release['tag_name']:
+        continue
+    formatted_date = datetime.strptime(release['published_at'], "%Y-%m-%dT%H:%M:%SZ").date().strftime("%Y-%m-%d")
+    print(
+            "<release version=\""+release['tag_name']+"\" url=\"https://github.com/MDK-Packs/tensorflow-pack/releases/download/"
+            +release['tag_name']+"/tensorflow.tensorflow-lite-micro."+release['tag_name']+".pack\" date=\""+formatted_date+"\">"
+            +release['body'] + "</release>"
+    )
