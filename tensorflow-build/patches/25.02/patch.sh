889f3751ed66c22027edc1dd3ad0e221009d9763
Evagan2018 <evangelos.ganotopoulos@arm.com>
2025-03-12 20:56:06 +0100

    Renamed main function to micro_test_main.

--- tensorflow-build/patches/25.02/patch.sh
+++ tensorflow-build/patches/25.02/patch.sh
@@ -3,8 +3,12 @@
 # Files         : ./tensorflow-pack/tensorflow-build/gen/build/tensorflow/lite/micro/testing/micro_test.h
 echo "Applying Patch 25-02.1"
 pwd
-sed -i 's/int main(int argc, char\*\* argv)/int main(void)/g' ./tensorflow-pack/tensorflow-build/gen/build/tensorflow/lite/micro/testing/micro_test.h
+# sed -i 's/int main(int argc, char\*\* argv)/int main(void)/g' ./tensorflow-pack/tensorflow-build/gen/build/tensorflow/lite/micro/testing/micro_test.h
+
+sed -i 's/int main(int argc, char\*\* argv)/int micro_test_main(int argc, char\*\* argv)/g' ./tensorflow-pack/tensorflow-build/gen/build/tensorflow/lite/micro/testing/micro_test.h
 cat ./tensorflow-pack/tensorflow-build/gen/build/tensorflow/lite/micro/testing/micro_test.h
 
-sed -i 's/int main(int argc, char\*\* argv)/int main(void)/g' ./tensorflow-pack/tensorflow-build/rel/ethos-u-main/core_software/tflite_micro/tensorflow/lite/micro/testing/micro_test.h
+
+# sed -i 's/int main(int argc, char\*\* argv)/int main(void)/g' ./tensorflow-pack/tensorflow-build/rel/ethos-u-main/core_software/tflite_micro/tensorflow/lite/micro/testing/micro_test.h
+sed -i 's/int main(int argc, char\*\* argv)/int micro_test_main(int argc, char\*\* argv)/g' ./tensorflow-pack/tensorflow-build/gen/build/tensorflow/lite/micro/testing/micro_test.h 
 cat ./tensorflow-pack/tensorflow-build/rel/ethos-u-main/core_software/tflite_micro/tensorflow/lite/micro/testing/micro_test.h
