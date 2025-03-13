e516a72f04014b7abfb0649ca35d763b0855c4b5
Evagan2018 <evangelos.ganotopoulos@arm.com>
2025-03-13 15:42:13 +0100

    Removed diagnosis blocks. Improved descriptions. Set ubuntu-24.04 for test_avh.yml

--- tensorflow-build/patches/25.02/patch.sh
+++ tensorflow-build/patches/25.02/patch.sh
@@ -1,6 +1,6 @@
-# Patch 25.02.1: Remove unnecessary files 
-# Approach      : Remove unnecessary files from ./tensorflow-pack/tensorflow-build/rel/mlplatform/core_software/tflite_micro/tensorflow/lite/micro/ and the file lists *.lst
-# Files         : hexdump_test.cpp
+# Patch 25.02.1 : Remove unnecessary files 
+# Approach      : Remove unnecessary files from folder /tensorflow-pack/tensorflow-build/gen/build/tensorflow/lite/micro/ and the file lists srcs.cmsis_nn.lst, srcs.ethos_u.lst, and srcs.lst
+# Files         : hexdump_test.cc, hexdump_test.cpp
 echo "Applying Patch 25-02.1"
 [ -f ./tensorflow-pack/tensorflow-build/gen/build/tensorflow/lite/micro/hexdump_test.cpp ] && rm ./tensorflow-pack/tensorflow-build/gen/build/tensorflow/lite/micro/hexdump_test.cpp
 grep -v 'hexdump_test.cc' ./tensorflow-pack/tensorflow-build/srcs.cmsis_nn.lst > temp && mv temp ./tensorflow-pack/tensorflow-build/srcs.cmsis_nn.lst
