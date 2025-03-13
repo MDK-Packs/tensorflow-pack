b4e1b2cf9fd08772e5c0dec0ee6c8f5f82a62da7
Evagan2018 <evangelos.ganotopoulos@arm.com>
2025-03-13 13:24:46 +0100

    Replaced file name hexdump_test.cpp with hexdump_test.cc.

--- tensorflow-build/patches/25.02/patch.sh
+++ tensorflow-build/patches/25.02/patch.sh
@@ -3,6 +3,6 @@
 # Files         : hexdump_test.cpp
 echo "Applying Patch 25-02.1"
 [ -f ./tensorflow-pack/tensorflow-build/gen/build/tensorflow/lite/micro/hexdump_test.cpp ] && rm ./tensorflow-pack/tensorflow-build/gen/build/tensorflow/lite/micro/hexdump_test.cpp
-grep -v 'hexdump_test.cpp' ./tensorflow-pack/tensorflow-build/srcs.cmsis_nn.lst > temp && mv temp ./tensorflow-pack/tensorflow-build/srcs.cmsis_nn.lst
-grep -v 'hexdump_test.cpp' ./tensorflow-pack/tensorflow-build/srcs.ethos_u.lst > temp && mv temp ./tensorflow-pack/tensorflow-build/srcs.ethos_u.lst
-grep -v 'hexdump_test.cpp' ./tensorflow-pack/tensorflow-build/srcs.lst > temp && mv temp ./tensorflow-pack/tensorflow-build/srcs.lst
+grep -v 'hexdump_test.cc' ./tensorflow-pack/tensorflow-build/srcs.cmsis_nn.lst > temp && mv temp ./tensorflow-pack/tensorflow-build/srcs.cmsis_nn.lst
+grep -v 'hexdump_test.cc' ./tensorflow-pack/tensorflow-build/srcs.ethos_u.lst > temp && mv temp ./tensorflow-pack/tensorflow-build/srcs.ethos_u.lst
+grep -v 'hexdump_test.cc' ./tensorflow-pack/tensorflow-build/srcs.lst > temp && mv temp ./tensorflow-pack/tensorflow-build/srcs.lst
