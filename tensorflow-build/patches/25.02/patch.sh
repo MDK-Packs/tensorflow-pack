cd6f7b2579ea21ee9becf38a4f8141de0e3a9b3d
Evagan2018 <evangelos.ganotopoulos@arm.com>
2025-03-13 10:10:16 +0100

    Removed hexdump_test.cpp

--- tensorflow-build/patches/25.02/patch.sh
+++ tensorflow-build/patches/25.02/patch.sh
@@ -1,14 +1,8 @@
-# Patch 25-02.1 : Error: L6200E: Symbol __ARM_use_no_argv multiply defined (by hexdump_test.o ...)
-# Approach      : Use sed to replace   int main(int argc, char** argv)  with int main(void)
-# Files         : ./tensorflow-pack/tensorflow-build/gen/build/tensorflow/lite/micro/testing/micro_test.h
+# Patch 25.02.1: Remove unnecessary files 
+# Approach      : Remove unnecessary files from ./tensorflow-pack/tensorflow-build/rel/mlplatform/core_software/tflite_micro/tensorflow/lite/micro/ and the file lists *.lst
+# Files         : hexdump_test.cpp
 echo "Applying Patch 25-02.1"
-pwd
-# sed -i 's/int main(int argc, char\*\* argv)/int main(void)/g' ./tensorflow-pack/tensorflow-build/gen/build/tensorflow/lite/micro/testing/micro_test.h
-
-sed -i 's/int main(int argc, char\*\* argv)/int micro_test_main(int argc, char\*\* argv)/g' ./tensorflow-pack/tensorflow-build/gen/build/tensorflow/lite/micro/testing/micro_test.h
-cat ./tensorflow-pack/tensorflow-build/gen/build/tensorflow/lite/micro/testing/micro_test.h
-
-
-# sed -i 's/int main(int argc, char\*\* argv)/int main(void)/g' ./tensorflow-pack/tensorflow-build/rel/ethos-u-main/core_software/tflite_micro/tensorflow/lite/micro/testing/micro_test.h
-sed -i 's/int main(int argc, char\*\* argv)/int micro_test_main(int argc, char\*\* argv)/g' ./tensorflow-pack/tensorflow-build/gen/build/tensorflow/lite/micro/testing/micro_test.h 
-cat ./tensorflow-pack/tensorflow-build/rel/ethos-u-main/core_software/tflite_micro/tensorflow/lite/micro/testing/micro_test.h
+[ -f ./tensorflow-pack/tensorflow-build/gen/build/tensorflow/lite/micro/hexdump_test.cpp ] && rm ./tensorflow-pack/tensorflow-build/gen/build/tensorflow/lite/micro/hexdump_test.cpp
+grep -v 'hexdump_test.cpp' ./tensorflow-pack/tensorflow-build/srcs.cmsis_nn.lst > temp && mv temp ./tensorflow-pack/tensorflow-build/srcs.cmsis_nn.lst
+grep -v 'hexdump_test.cpp' ./tensorflow-pack/tensorflow-build/srcs.ethos_u.lst > temp && mv temp ./tensorflow-pack/tensorflow-build/srcs.ethos_u.lst
+grep -v 'hexdump_test.cpp' ./tensorflow-pack/tensorflow-build/srcs.lst > temp && mv temp ./tensorflow-pack/tensorflow-build/srcs.lst
